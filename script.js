ff1f6ba27d7dae2479814f01c988998c7c5cb2ab
Piotr Wereszczyński <6324814+YourSenseiCreeper@users.noreply.github.com>
2024-04-14 19:35:45 +0200

    Prototype version

--- script.js
+++ script.js
@@ -35,6 +35,31 @@ class Answeres {
     }
 }
 
+class Result {
+    constructor(type, value) {
+        this.type = type;
+        this.value = value;
+    }
+}
+
+class ResultType {
+    constructor(type, description) {
+        this.type = type;
+        this.description = description;
+    }
+}
+
+var types = [
+    new ResultType('PR - Praktyczny Realizator', 'To osoba praktyczna, operatywna, nastawiona na konkretne działanie. Potrafi ocenić realność i szanse powodzenia projektu oraz wdrażać plany i marzenia w czyn. Jest doskonałym wykonawcą projektów wymyślonych przez innych. W pracy zespołowej koncentruje się na organizowaniu pracy i praktycznym wykorzystaniu zasobów posiadanych przez zespół (ludzie i ich umiejętności, narzędzia, środki, metody).'),
+    new ResultType('KL - Koordynujący Lider', 'To ktoś potrafiący zwiększyć efektywność zespołu poprzez właściwą koordynację działań jej członków. Istotą jego postępowania jest zachęta, przekonywanie, godzenie kontrowersji, słuchanie opinii innych. Ma zdolność wyciągania i angażowania w działanie wszystkich członków zespołu tak, aby jak najlepiej wykorzystać ich indywidualne możliwości.'),
+    new ResultType('AK - Ambitny Komendant', 'To osoba o wysokiej motywacji i potrzebie osiągnięć próbująca zmusić zespół do stawiania coraz ambitniejszych celów i bardziej intensywnego działania. Jako typ ambitnego lidera gromi innych za opieszałość, spoczywanie na laurach i bezwładność. Jednak  w odróżnieniu od Koordynatora istotą jego działania jest ostra krytyka, wymuszanie i obrażanie, co rodzi opór i wywołuje sytuacje konfliktowe.'),
+    new ResultType('KP - Kreatywny Pomysłodawca', 'Jest to osoba kreatywna, opierająca się na wyobraźni i niestandardowym podejściu do zagadnień. Lubi pracować w samotności i skupiać się na poszukiwaniu nowatorskich rozwiązań problemów przed którymi stanęła grupa. Często w swoich pomysłach nie liczy się z realiami obowiązujących procedur i ograniczeń, za co jest krytykowana przez praktycznie nastawionych członków zespołu.'),
+    new ResultType('WŁ - Wszędobylski Łącznik', 'To osoba posiadająca rozliczne kontakty, dzięki którym potrafi dotrzeć do różnych miejsc i informacji przydatnych dla zespołu. Jest osobą niezwykle uzdolnioną interpersonalnie, entuzjastyczną i przyjazną, dzięki czemu jest bardzo dobrze odbierana przez innych. Ma zdolności do kontaktowania ludzi i uczestniczenia w negocjacjach. Jest też bardzo dobrym ambasadorem grupy na zewnątrz.'),
+    new ResultType('RA - Racjonalny Analityk', 'Jest to osoba o głębokiej wnikliwości i wysokiej inteligencji, krytyczna i obiektywna, z natury małomówna. Zwykle przyjmuje w zespole pozycję osoby stojącej z boku  i obserwującej. Wkracza wtedy, gdy ma zapaść rozstrzygająca decyzja. Jest nieoceniona w bezstronnej, rzeczowej analizie faktów, która pomaga grupie w wypracowaniu najlepszej decyzji, w sytuacji burzliwego ścierania się przeciwstawnych pomysłów.'),
+    new ResultType('DZ - Dusza Zespołu', 'Jest to osoba ciepłą, łatwo nawiązująca kontakty, wrażliwa na emocje innych. Najważniejsze jest dla niej utrzymanie dobrej atmosfery w zespole. Dzięki swym zdolnościom dyplomatycznym pomaga neutralizować problemy interpersonalne w zespole, likwidować napięcia i łagodzić konflikty. Szczególnie przydatna w sytuacjach wewnętrznej walki pomiędzy członkami zespołu, nie konstruktywnej dyskusji, nadmiernej krytyki czy nagonki na jednego z członków zespołu.'),
+    new ResultType('FP - Finiszujący Perfekcjonista', 'To osoba bardzo skrupulatna i zdyscyplinowana, skoncentrowana na jakości i metodycznym postępowaniu z dbałością o szczegóły. Czuje się odpowiedzialna za przebieg i wynik pracy zespołu. Stale zadręcza się niepokojem o to, czy wszystko idzie dobrze i stara się zapobiegać katastrofom poprzez sprawdzanie detali. Lubi doprowadzać sprawy do końca i „dopinać na ostatni guzik”'),
+]
+
 var questions = [
     // 1
     new Questions("Myślę, że na mój wkład w pracę zespołu składa się...",
@@ -136,15 +161,15 @@ var answeres = [
 
 function slide(groupNumber, answerNumber, value) {
     let numberValue = Number(value);
-    if (numberValue <= 0)
-        return;
+    // if (numberValue <= 0)
+    //     return;
 
     let answerGroup = answeres[groupNumber];
     answerGroup.set(answerNumber, numberValue);
 
     let available = answerGroup.getAvailable();
     // przeliczanie ograniczeń dla suwaków
-    for(let i = 1; i < 8; i++) {
+    for(let i = 0; i < 8; i++) {
         let sliderDom = document.getElementById(`slider-${groupNumber}-${i}`);
         let currentValue = Number(sliderDom.value);
         let widthWithAvailable = Math.max(currentValue+available,available);
@@ -155,11 +180,101 @@ function slide(groupNumber, answerNumber, value) {
     }
 }
 
+function calculateResults() {
+    let a1 = answeres[0];
+    let a2 = answeres[1];
+    let a3 = answeres[2];
+    let a4 = answeres[3];
+    let a5 = answeres[4];
+    let a6 = answeres[5];
+    let a7 = answeres[6];
+
+    let pr = a1.g + a2.a + a3.h + a4.d + a5.b + a6.f + a7.e;
+    let kl = a1.d + a2.b + a3.a + a4.h + a5.f + a6.c + a7.g;
+    let ak = a1.f + a2.e + a3.c + a4.b + a5.d + a6.g + a7.a;
+    let kp = a1.c + a2.g + a3.d + a4.e + a5.h + a6.a + a7.f;
+    let wl = a1.a + a2.c + a3.f + a4.g + a5.e + a6.h + a7.d;
+    let ra = a1.h + a2.d + a3.g + a4.c + a5.a + a6.e + a7.b;
+    let dz = a1.b + a2.f + a3.e + a4.a + a5.c + a6.b + a7.h;
+    let fp = a1.e + a2.h + a3.b + a4.f + a5.g + a6.d + a7.c;
+    
+    let resultWithTypes = [
+        new Result('PR - Praktyczny Realizator', pr),
+        new Result('KL - Koordynujący Lider', kl),
+        new Result('AK - Ambitny Komendant', ak),
+        new Result('KP - Kreatywny Pomysłodawca', kp),
+        new Result('WŁ - Wszędobylski Łącznik', wl),
+        new Result('RA - Racjonalny Analityk', ra),
+        new Result('DZ - Dusza Zespołu', dz),
+        new Result('FP - Finiszujący Perfekcjonista', fp),
+    ];
+    let ordered = resultWithTypes.sort((a, b) => b.value - a.value);
+
+    let questionsDiv = document.getElementById('questions');
+    questionsDiv.style.display = 'none';
+
+    let introductionDiv = document.getElementById('introduction');
+    introductionDiv.style.display = 'none';
+
+    window.scrollTo(0, 0);
+
+    let resultsTableHtml = generateResultsTable(ordered);
+    let definitionsTableHtml = generateDefinitions();
+
+    let resultsDiv = document.getElementById('results');
+    resultsDiv.innerHTML = '<h1>Wyniki</h1>' + resultsTableHtml + definitionsTableHtml;
+}
+
+function generateDefinitions() {
+    /*
+    <div class="definitions">
+        <div class="single-definition">
+            <h2>KL - Koordynujący Lider</h2>
+            <p>Opis</p>
+        </div>
+    </div> 
+    */
+
+    let definitionsHtml = '';
+    types.forEach(type => {
+        definitionsHtml += `<div class="single-definition"><h2>${type.type}</h2><p>${type.description}</p></div>`;
+    });
+
+    let definitionsFinalHtml = `<div class="definitions">${definitionsHtml}</div>`;
+    return definitionsFinalHtml;
+}
+
+function generateResultsTable(orderedResults) {
+    /*
+    <table class="results-table">
+        <thead>
+            <th>Rola</th>
+            <th>Punkty</th>
+            <th>Procent</th>
+        </thead>
+        <tbody>
+            <tr>
+                <td>KL - Koordynujący Lider</td>
+                <td>15</td>
+                <td>55%</td>
+            </tr>
+        </tbody>
+    </table>
+    */
+   let resultsTableContent = '';
+   orderedResults.forEach(result => {
+    resultsTableContent += `<tr><td>${result.type}</td><td>${result.value}</td><td>${Math.round(result.value / 70 * 100)}%</td></tr>`;
+   });
+   let resultsTableHtml = `<table class="results-table"><thead><th>Rola</th><th>Punkty</th><th>Procent</th></thead><tbody>${resultsTableContent}</tbody></table>`;
+   return resultsTableHtml;
+}
+
 function generateQuestionsHtml() {
     let sumHtml = '';
     for(let i = 0; i < questions.length; i++)
         sumHtml += getQuestionHtml(i);
 
+    sumHtml += "<div class='submit-section'><button class='submit' onclick='calculateResults()'>Wyświetl wyniki</button></div>";
     return sumHtml
 }
 
